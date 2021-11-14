668be448a7c68c3b8fd84b93f2db787e758678f7
Jeffery McNeil <jeffmc825@gmail.com>
2021-11-14 11:06:11 -0600

    added javaScript elements

---
+++ script.js
@@ -0,0 +1,9 @@
+const enbyFlag = document.getElementById('enbyFlag');
+enbyFlag.addEventListener('click', function () {
+    alert('Non-Binary Pride!');
+});
+
+const panFlag = document.getElementById('panFlag');
+panFlag.addEventListener('click', function () {
+    alert('Pansexual Pride!');
+});
